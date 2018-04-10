3cecc9d6b074eacce9b8716e95dc72542f150ff4
Asia Hoe <coding@asiahoe.com>
2018-04-09 23:53:32 -0400

    Add makeGrid and paintGrid functions.

--- designs.js
+++ designs.js
@@ -1,24 +1,49 @@
-// Select color input
-// Select size input
+const table = '#pixelCanvas';
+// Set target ID to variable.
 
-// When size is submitted by the user, call makeGrid()
+const row = '<tr></tr>';
+// Set row elements to variable.
 
-let color = $('#colorPicker').val();
-let startRow = "<tr>";
-let endRow = "</tr>";
-let cell = "<td>&nbsp;</td>";
+const cell = '<td></td>';
+// Set cell elements to variable.
 
-$('#sizePicker').submit(function makeGrid () {
-  let height = $('#inputHeight').val();
-  let width = $('#inputWidth').val();
-  for (let m = 1; m <= height; m++) {
-    $('#pixelCanvas').append('<h3>Heading</h3>');
-    for (let n = 1; n <= width; n++) {
-      $('#pixelCanvas').append('<p>Paragraph</p>');
+$('#sizePicker').submit(function makeGrid (event, height, width) {
+// When the sizePicker form is submitted, run the makeGrid function.
+// The makeGrid function takes the parameters event, height, and width.
+
+  event.preventDefault();
+  // Prevent auto-refresh from deleting grid as it's displayed.
+
+  $(table).empty();
+  // Clear the grid on submit, if there is one.
+
+  height = $('#inputHeight').val();
+  // Fetch the input value of height.
+
+  width = $('#inputWidth').val();
+  // Fetch the input value of width.
+
+  for (let m = 0; m < height; m++) {
+  // Outer loop defined by height.
+
+    $(table).append(row);
+    // Append a row to the table m times.
+
+    for (let n = 0; n < width; n++) {
+    // Inner loop defined by width.
+
+      $('tr').last().append(cell);
+      // Append n cells to the last row m times.
     }
   }
 })
 
-// $('#colorPicker').change(function () {
-//   $('td').css('background-color', color);
-// })
+$(table).on('click', 'td', function paintGrid () {
+// When the table is clicked on, run the paintGrid function on the target cell.
+
+  let color = $('#colorPicker').val();
+  // Grab the input color each time, in case it's changed.
+
+  $(this).css('background-color', color);
+  // Set this cell's background to the input color.
+});

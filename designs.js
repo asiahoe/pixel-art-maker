d01b8d287d8b2cce8b6f6e7a54bff143d518e76c
Asia Hoe <coding@asiahoe.com>
2018-04-09 21:16:33 -0400

    Add initial ideation.

--- designs.js
+++ designs.js
@@ -3,8 +3,22 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+let color = $('#colorPicker').val();
+let startRow = "<tr>";
+let endRow = "</tr>";
+let cell = "<td>&nbsp;</td>";
 
-// Your code goes here!
+$('#sizePicker').submit(function makeGrid () {
+  let height = $('#inputHeight').val();
+  let width = $('#inputWidth').val();
+  for (let m = 1; m <= height; m++) {
+    $('#pixelCanvas').append('<h3>Heading</h3>');
+    for (let n = 1; n <= width; n++) {
+      $('#pixelCanvas').append('<p>Paragraph</p>');
+    }
+  }
+})
 
-}
+// $('#colorPicker').change(function () {
+//   $('td').css('background-color', color);
+// })
